aab91a8ecd0543ff0bdfbd7bd76dd97f3728c5d8
torisasamiii <bonzin0904@yahoo.co.jp>
2022-12-13 19:11:41 +0900

    add LowLink and TwoEdgeConnectedComponents

---
+++ src/TwoEdgeConnectedComponents.hpp
@@ -0,0 +1,55 @@
+// 二重辺連結成分分解（分解後は森になる）
+// 計算量 O(n+m)
+
+#include "LowLink.hpp"
+
+template <bool directed = false>
+struct Two_Edge_Connected_Components : Low_Link<directed> {
+    using L = Low_Link<directed>;
+    vector<int> c;
+    const int n;
+
+    Two_Edge_Connected_Components(int n) : L(n), c(n), n(n) {}
+
+    int dfs(int now, int p, int k) {
+        if (p != -1 && this->o[p] >= this->l[now]) {
+            c[now] = c[p];
+        } else {
+            c[now] = k++;
+        }
+        for (auto &[to, id] : this->es[now]) {
+            if (c[to] == -1)
+                k = dfs(to, now, k);
+        }
+        return k;
+    }
+
+    // 構築し、縮約されたグラフを返す
+    vector<vector<int>> decompose() {
+        this->build();
+        fill(all(c), -1);
+        int k = 0;
+        rep(i, n) {
+            if (c[i] == -1)
+                k = dfs(i, -1, k);
+        }
+        vector<vector<int>> ret(k);
+        vector<int> isb(this->m, 0);
+        each(e, this->bridge) isb[e]++;
+        rep(i, n) {
+            for (auto &[to, id] : this->es[i]) {
+                if (isb[id]-- > 0) {
+                    ret[c[i]].eb(c[to]);
+                    if (!directed)
+                        ret[c[to]].eb(c[i]);
+                }
+            }
+        }
+        return ret;
+    }
+
+    // 頂点iが属する連結成分の番号
+    int operator[](int i) const {
+        return c[i];
+    }
+};
