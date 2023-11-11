1d2903501214b936ac9f13226a7862e1c7b1bf25
tokusakurai <Togo.39rai@gmail.com>
2023-11-11 10:06:24 +0900

    [update] rolling hash mul

--- src/RollingHash_ull.hpp
+++ src/RollingHash_ull.hpp
@@ -4,17 +4,25 @@
 using ull = unsigned long long;
 const ull mod = (1LL << 61) - 1;
 
-ull h_mod(ull x) {
+namespace Hash {
+ull getmod(ull x) {
     ull ret = (x >> 61) + (x & mod);
-    return ret - (ret >= mod ? mod : 0);
+    if (ret >= mod) ret -= mod;
+    return ret;
 }
 
-ull h_mul(ull x, ull y) {
-    x = h_mod(x), y = h_mod(y);
-    ull x1 = x >> 31, x2 = x & ((1LL << 31) - 1), y1 = y >> 31, y2 = y & ((1LL << 31) - 1);
-    ull z = x1 * y2 + x2 * y1, z1 = z >> 30, z2 = z & ((1 << 30) - 1);
-    return h_mod(x1 * y1 * 2 + x2 * y2 + z1 + (z2 << 31));
+ull mul(ull x, ull y) {
+    x = getmod(x), y = getmod(y);
+    ull x1 = x >> 31;
+    ull x2 = x & ((1LL << 31) - 1);
+    ull y1 = y >> 31;
+    ull y2 = y & ((1LL << 31) - 1);
+    ull z = x1 * y2 + x2 * y1;
+    ull z1 = z >> 30;
+    ull z2 = z & ((1LL << 30) - 1);
+    return getmod(x1 * y1 * 2 + x2 * y2 + z1 + (z2 << 31));
 }
+} // namespace Hash
 
 // 0 が含まれないように
 template <typename T>
@@ -27,15 +35,15 @@ struct RollingHash {
     RollingHash(const T &s, ull b = 887446835055281585) : n(sz(s)), b(b) {
         h.assign(n + 1, 0), p.assign(n + 1, 1);
         rep(i, n) {
-            p[i + 1] = h_mul(p[i], b);
-            h[i + 1] = h_mul(h[i], b) + s[i];
+            p[i + 1] = Hash::mul(p[i], b);
+            h[i + 1] = Hash::mul(h[i], b) + s[i];
             if (h[i + 1] >= mod) h[i + 1] -= mod;
         }
     }
 
     // 文字列の [l,r) の部分のハッシュ値
     ull get_hash(int l, int r) const {
-        ull ret = h[r] + mod - h_mul(h[l], p[r - l]);
+        ull ret = h[r] + mod - Hash::mul(h[l], p[r - l]);
         return ret - (ret >= mod ? mod : 0);
     }
 
