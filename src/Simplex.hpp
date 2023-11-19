cbe1abb7d6de8b9d4560168fb243c67340ed3c8e
tokusakurai <Togo.39rai@gmail.com>
2023-11-19 14:18:31 +0900

    [update] Simplex

--- src/Simplex.hpp
+++ src/Simplex.hpp
@@ -4,17 +4,17 @@
 // Maximize cx s.t. Ax <= b, x >= 0
 // Implementation idea: https://kopricky.github.io/code/Computation_Advanced/simplex.html
 // Refer to https://hitonanode.github.io/cplib-cpp/combinatorial_opt/simplex.hpp
-template <typename Float = double, int LOGEPS = 20, bool Randomize = false>
+template <typename Float = double, int LOGEPS = 20>
 struct Simplex {
-    const Float EPS = Float(1.0) / (1LL << LOGEPS);
+    const Float EPS = 1.0 / (1LL << LOGEPS);
     int n, m;
     vector<int> shuffle_idx;
     vector<int> idx;
     vector<vector<Float>> mat;
     int i_ch, j_ch;
 
-    void _initialize(const vector<vector<Float>> &A, const vector<Float> &b, const vector<Float> &c) {
-        n = c.size(), m = A.size();
+    void _initialize(vector<vector<Float>> A, vector<Float> b, vector<Float> c) {
+        n = sz(c), m = sz(A);
 
         mat.assign(m + 2, vector<Float>(n + 2));
         i_ch = m;
@@ -30,23 +30,21 @@ struct Simplex {
         iota(all(idx), 0);
     }
 
-    inline Float abs_(Float x) noexcept { return x > -x ? x : -x; }
-
     void _solve() {
         vector<int> jupd;
         for (nb_iter = 0, j_ch = n;; nb_iter++) {
             if (i_ch < m) {
                 swap(idx[j_ch], idx[i_ch + n + 1]);
-                mat[i_ch][j_ch] = Float(1) / mat[i_ch][j_ch];
+                mat[i_ch][j_ch] = 1.0 / mat[i_ch][j_ch];
                 jupd.clear();
                 rep(j, n + 2) {
                     if (j != j_ch) {
                         mat[i_ch][j] *= -mat[i_ch][j_ch];
-                        if (abs_(mat[i_ch][j]) > EPS) jupd.push_back(j);
+                        if (abs(mat[i_ch][j]) > EPS) jupd.eb(j); // abs() の括弧の位置に要注意
                     }
                 }
                 rep(i, m + 2) {
-                    if (abs_(mat[i][j_ch]) < EPS || i == i_ch) continue;
+                    if (abs(mat[i][j_ch]) < EPS || i == i_ch) continue; // abs() の括弧の位置に要注意
                     each(j, jupd) mat[i][j] += mat[i][j_ch] * mat[i_ch][j];
                     mat[i][j_ch] *= mat[i_ch][j_ch];
                 }
@@ -55,7 +53,7 @@ struct Simplex {
             j_ch = -1;
             rep(j, n + 1) {
                 if (j_ch < 0 || idx[j_ch] > idx[j]) {
-                    if (mat[m + 1][j] > EPS || (abs_(mat[m + 1][j]) < EPS && mat[m][j] > EPS)) j_ch = j;
+                    if (mat[m + 1][j] > EPS || (abs(mat[m + 1][j]) < EPS && mat[m][j] > EPS)) j_ch = j; // abs() の括弧の位置に要注意
                 }
             }
             if (j_ch < 0) break;
@@ -72,18 +70,20 @@ struct Simplex {
                     }
                 }
             }
+
             if (i_ch < 0) {
                 is_infty = true;
                 break;
             }
         }
+
         if (mat[m + 1][n + 1] < -EPS) {
             infeasible = true;
             return;
         }
         x.assign(n, 0);
-        for (int i = 0; i < m; i++) {
-            if (idx[n + 1 + i] < n) x[idx[n + 1 + i]] = mat[i][n + 1];
+        rep(i, m) {
+            if (idx[n + 1 + i] < n) x[idx[n + 1 + i]] = mat[i][n + i];
         }
         ans = mat[m][n + 1];
     }
@@ -96,58 +96,7 @@ struct Simplex {
 
     Simplex(vector<vector<Float>> A, vector<Float> b, vector<Float> c) {
         is_infty = infeasible = false;
-
-        //-------------------------------基本的に使わない-----------------------------
-        if (Randomize) {
-            mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-            vector<pair<vector<Float>, Float>> Abs;
-            rep(i, sz(A)) Abs.emplace_back(A[i], b[i]);
-            shuffle(all(Abs), rng);
-            A.clear(), b.clear();
-            each(Ab, Abs) {
-                A.emplace_back(Ab.first);
-                b.emplace_back(Ab.second);
-            }
-
-            shuffle_idx.resize(sz(c));
-            iota(all(shuffle_idx), 0);
-            shuffle(all(shuffle_idx), rng);
-            auto Atmp = A;
-            auto ctmp = c;
-            rep(i, sz(A)) {
-                rep(j, sz(A[i])) {
-                    A[i][j] = Atmp[i][shuffle_idx[j]]; //
-                }
-            }
-            rep(j, sz(c)) c[j] = ctmp[shuffle_idx[j]];
-        }
-        //---------------------------------------------------------------------------
-
         _initialize(A, b, c);
         _solve();
-
-        //-------------------------------基本的に使わない-----------------------------
-        if (Randomize && sz(x) == sz(c)) {
-            auto xtmp = x;
-            rep(j, sz(c)) x[shuffle_idx[j]] = xtmp[j];
-        }
-        //---------------------------------------------------------------------------
     }
-
-    //-------------------------------基本的に使わない-----------------------------
-    static void dual(vector<vector<Float>> &A, vector<Float> &b, vector<Float> &c) {
-        int n = sz(b), m = sz(c);
-        vector<vector<Float>> At(m, vector<Float>(n));
-        rep(i, n) {
-            rep(j, m) {
-                At[j][i] = -A[i][j]; //
-            }
-        }
-        A = At;
-        rep(i, n) b[i] = -b[i];
-        rep(j, m) c[j] = -c[j];
-        b.swap(c);
-    }
-    //---------------------------------------------------------------------------
 };
