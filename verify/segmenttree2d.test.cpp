957f6b92d1a58549fb011a1850dee6d6ba591116
tokusakurai <Togo.39rai@gmail.com>
2023-11-23 11:40:39 +0900

    [create] 2d segtree

--- verify/segmenttree2d.test.cpp
+++ verify/segmenttree2d.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/point_add_rectangle_sum"
 
-#include "../src/BIT2D.hpp"
-#include "../src/BIT.hpp"
+#include "../src/SegmentTree2D.hpp"
+#include "../src/SegmentTree.hpp"
 
 int main() {
     int N, Q;
@@ -10,11 +10,13 @@ int main() {
     vector<int> x(N), y(N);
     vector<ll> w(N);
 
-    BIT2D<ll, int> bit;
+    auto f = [](ll x, ll y) { return x + y; };
+
+    SegmentTree2D<ll, int> seg(f, 0);
 
     rep(i, N) {
         cin >> x[i] >> y[i] >> w[i];
-        bit.insert(x[i], y[i]);
+        seg.insert(x[i], y[i]);
     }
 
     vector<int> t(Q), lx(Q), ly(Q), rx(Q), ry(Q);
@@ -24,21 +26,21 @@ int main() {
         cin >> t[i];
         if (t[i] == 0) {
             cin >> lx[i] >> ly[i] >> c[i];
-            bit.insert(lx[i], ly[i]);
+            seg.insert(lx[i], ly[i]);
         } else {
             cin >> lx[i] >> ly[i] >> rx[i] >> ry[i];
         }
     }
 
-    bit.build();
+    seg.build();
 
-    rep(i, N) bit.add(x[i], y[i], w[i]);
+    rep(i, N) seg.update(x[i], y[i], w[i], true);
 
     rep(i, Q) {
         if (t[i] == 0) {
-            bit.add(lx[i], ly[i], c[i]);
+            seg.update(lx[i], ly[i], c[i], true);
         } else {
-            cout << bit.query(lx[i], rx[i], ly[i], ry[i]) << '\n';
+            cout << seg.query(lx[i], rx[i], ly[i], ry[i]) << '\n';
         }
     }
 }
